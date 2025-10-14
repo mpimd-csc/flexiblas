f0316a771929e3b94c7481ca3b39044d345f6c3b
Martin Köhler <koehlerm@mpi-magdeburg.mpg.de>
2025-10-14 07:14:46 +0000

    Release v3.4.84

--- debian/bootstrap_debian.sh
+++ debian/bootstrap_debian.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env sh
-export OS_DIST=$(grep ^VERSION_CODENAME /etc/os-release | cut -d'=' -f2 | sed 's/\"//gI')
+export OS_ID=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 | sed 's/\"//gI')
+
+echo $OS_ID
 cd debian
-if [ -f control.${OS_DIST} ]; then
-    ln -sf control.${OS_DIST} control
+if [ "${OS_ID}" = "ubuntu" ]; then
+    ubuntu_version=$(grep ^VERSION_ID /etc/os-release | cut -d'=' -f 2 | sed 's/\"//gI' )
+    ubuntu_version_major=$(echo $ubuntu_version | cut -d. -f 1)
+    ubuntu_version_minor=$(echo $ubuntu_version | cut -d. -f 2)
+    ubuntu_version_int=$(($ubuntu_version_major*100 + $ubuntu_version_minor))
+    if [ ${ubuntu_version_int} -gt 2504 ]; then
+        ln -sf control.no_atlas control
+    else
+        ln -sf control.default control
+    fi
 else
-    ln -sf control.default control
+    export OS_DIST=$(grep ^VERSION_CODENAME /etc/os-release | cut -d'=' -f2 | sed 's/\"//gI')
+    debian_version=$(grep ^VERSION_ID /etc/os-release | cut -d'=' -f 2 | sed 's/\"//gI' )
+    if [ ${debian_version} -gt 12 ]; then
+        ln -sf control.no_atlas control
+    else
+        ln -sf control.default control
+    fi
 fi
